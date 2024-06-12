311df7a454ba38be88b718a3d653def1dc8e9438
stealthscripter <nicolasayele@gmail.com>
2024-06-12 16:59:26 +0300

    traverse bkg error

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,65 +1,63 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 using System.Xml.Serialization;
 
 namespace ConsoleApp1 {
-    public class RsaEnc{
-    
+    public class RsaEnc {
+
         private static RSACryptoServiceProvider csp = new RSACryptoServiceProvider(2048);
         private RSAParameters _privatekey;
-        
         private RSAParameters _publickey;
 
-        public RsaEnc(){
-            
+        public RsaEnc() {
             _privatekey = csp.ExportParameters(true);
             _publickey = csp.ExportParameters(false);
         }
-        public string PublicKeyString(){
+
+        public string PublicKeyString() {
             var sw = new StringWriter();
             var xs = new XmlSerializer(typeof(RSAParameters));
-            xs.Serialize(sw,_publickey);
-
+            xs.Serialize(sw, _publickey);
             return sw.ToString();
         }
-        public string Encrypt(string plainText){
+
+        public string Encrypt(string plainText) {
             csp = new RSACryptoServiceProvider();
             csp.ImportParameters(_publickey);
-
             var data = Encoding.Unicode.GetBytes(plainText);
-            var cypher = csp.Encrypt(data , false);
+            var cypher = csp.Encrypt(data, false);
             return Convert.ToBase64String(cypher);
         }
-        public string Decrypt(string cypherText){
+
+        public string Decrypt(string cypherText) {
             var dataBytes = Convert.FromBase64String(cypherText);
             csp.ImportParameters(_privatekey);
             var plainText = csp.Decrypt(dataBytes, false);
-            
             return Encoding.Unicode.GetString(plainText);
         }
-
     }
 
-    class Program{
-        static void Main(string[] args){
+    class Program {
+        static void Main(string[] args) {
             RsaEnc rs = new();
             string cypher = String.Empty;
-        
-            Console.WriteLine($"PublicKey: \n {rs.PublicKeyString()}\n");
-            
-            Console.WriteLine("Enter Your Text to Encrypt: \n");
 
+            Console.WriteLine($"PublicKey: \n{rs.PublicKeyString()}\n");
+
+            Console.WriteLine("Enter Your Text to Encrypt: ");
             var text = Console.ReadLine();
-            if(text != String.Empty){
-                cypher = rs.Encrypt(cypher);
-                
-                Console.WriteLine($"Cypher Text: {cypher} \n");
+
+            if (!string.IsNullOrEmpty(text)) {
+                cypher = rs.Encrypt(text);
+                Console.WriteLine($"Cypher Text: {cypher}\n");
             }
 
             Console.WriteLine("Press enter to decrypt");
             Console.ReadLine();
-            var plainText = rs.Decrypt(text);
-            Console.WriteLine("Decrypt Text \n");
+            var plainText = rs.Decrypt(cypher);
+            Console.WriteLine("Decrypted Text: ");
             Console.WriteLine(plainText);
             Console.ReadLine();
         }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e7f8c1db387a37bffbabb21926f008b5c131b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+000040bd4ab0647092a800a59a4577d51a88cbda")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
